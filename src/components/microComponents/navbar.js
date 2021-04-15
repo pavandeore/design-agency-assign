94b05ff8a77c82fc4a4de05732e57ac09b935703
pawan deore <pavandeore90@gmail.com>
2021-04-15 15:19:24 +0530

    navbar fix

--- src/components/microComponents/navbar.js
+++ src/components/microComponents/navbar.js
@@ -8,32 +8,33 @@ function navbar() {
         <div>
             <nav className="navbar navbar-expand-md navbar-light shadow-effect ">
                 <div className="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
-                <a className="navbar-brand ml-4" href="#">
-                    <img alt="img" src={logo} width="40" height="40" alt="" />
-                </a>
+                    <a className="navbar-brand ml-4" href="#">
+                        <img alt="img" src={logo} width="40" height="40" alt="" />
+                    </a>
+                    <div className="mx-auto order-0">
+                        <ul id="navbar" className="navbar-nav mx-auto">
+                            <li className="nav-item active">
+                                <a className="nav-link mx-2 text-dark font-weight-bold" href="#">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link mx-2 text-dark font-weight-bold" href="#">About</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link mx-2 text-dark font-weight-bold" href="#">Work</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link mx-2 text-dark font-weight-bold" href="#">Product</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link mx-2 text-dark font-weight-bold" href="#">Projects</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link mx-2 text-dark font-weight-bold" href="#">Blog</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                <div className="mx-auto order-0">
-                    <ul id="navbar" className="navbar-nav mx-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link mx-2 text-dark font-weight-bold" href="#">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link mx-2 text-dark font-weight-bold" href="#">About</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link mx-2 text-dark font-weight-bold" href="#">Work</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link mx-2 text-dark font-weight-bold" href="#">Product</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link mx-2 text-dark font-weight-bold" href="#">Projects</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link mx-2 text-dark font-weight-bold" href="#">Blog</a>
-                        </li>
-                    </ul>
-                </div>
+                
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
                         <span className="navbar-toggler-icon"></span>
                 </button>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import $ from 'jquery';
+import Popper from 'popper.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 ReactDOM.render(
   
